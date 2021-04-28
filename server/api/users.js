d53c69da0fd164c77881a6128f50a6ce6e77854e
manhdg98 <manhdg98@gmail.com>
2021-04-28 23:10:24 +0700

    - Update api user for updapte-password and get all users

--- server/api/users.js
+++ server/api/users.js
@@ -1,78 +1,83 @@
 // TODO: make /users/:username or email api endpoints
 
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const { User, Curriculum } = require('@db')
-const bcrypt = require('bcrypt')
-const { hashPassword } = require('../utils/auth')
+const { User, Curriculum } = require("@db");
+const bcrypt = require("bcrypt");
+const { hashPassword } = require("../utils/auth");
 
-const router = express.Router()
+const router = express.Router();
 
-if (process.env.NODE_ENV !== 'production') {
-  mongoose.set('debug', true)
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
 }
 
-router.route('/:id/curricula')
+router.route("/:id/update-password").post(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (newPassword.length >= 8) {
+    res.send(400);
+  }
+
+  const { id } = req.params;
+  const user = await User.findById(id);
+  console.log(
+    "user",
+    user,
+    "user-password",
+    user.password,
+    "oldPassword",
+    oldPassword
+  );
+
+  const isValid = await bcrypt.compare(oldPassword, user.password);
+
+  console.log("manhnt", isValid);
+  if (!isValid) {
+    res.status(400).send("Invalid Password");
+  }
+
+  user.password = await hashPassword(newPassword);
+  await user.save();
+
+  res.send(200);
+});
+
+router
+  .route("/:id")
   .get(async (req, res) => {
-    const userId = req.params.id
-
-    const curricula = await Curriculum.paginate({ createdBy: userId }, {
-      page: parseInt(req.query.page) || 1,
-      limit: 5
-    })
-    res.send(curricula)
-  })
-
-router.route('/:id/update-password')
-  .post(async (req, res) => {
-    const { oldPassword, newPassword } = req.body
-
-    if (newPassword.length >= 8) {
-      res.send(400)
-    }
-    
-    const id = req.params.id
-    const user = await User.findById(id)
-
-    const isValid = await bcrypt.compare(oldPassword, user.password)
-    if (!isValid) {
-      res.status(400).send('Invalid Password')
-    }
-
-    user.password =  await hashPassword(newPassword)
-    await user.save()
-
-    res.send(200)
-  })  
-
-router.route('/:id')
-  .get(async (req, res) => {
-    const userId = req.params.id
-    const user = await User.findById(userId)
-    res.send(user)
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    res.send(user);
   })
   .patch(async (req, res) => {
-    const userId = req.params.id
-    const { username, email } = req.body
+    const userId = req.params.id;
+    const { username, email } = req.body;
 
-    const user = await User.findById(userId)
+    const user = await User.findById(userId);
 
-    if (email) user.email = email
-    if (username) user.username = username
+    if (email) user.email = email;
+    if (username) user.username = username;
 
-    await user.save()
+    await user.save();
 
-    res.send(201)
+    res.send(201);
   })
-  .delete(async (req, res) => {
+  .delete(async (req, res) => {});
 
-  })
+router.route("/").get(async (req, res) => {
+  // const users = await User.findAll()
+  // res.send(users)
+  User.find({}, (err, users) => {
+    let userMap = {};
 
-router.route('/')
-  .get(async (req, res) => {
-    const users = await User.findAll()
-    res.send(users)
-  })
+    users.forEach((user) => {
+      userMap[user._id] = user;
+    });
 
-module.exports = router
+    res.send(userMap);
+  });
+});
+
+module.exports = router;
