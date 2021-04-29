be7b96f0170cda9be639cd4e9b33588da867f900
Tuanh4198 <nguyenanh.it.4198@gmail.com>
2021-04-29 22:51:14 +0700

    add template authen (login, register, verify)

---
+++ social-front/src/components/authen/Verify.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Verify = props => {
+  return (
+    <div className="logout-f">
+      <h4>
+        {" "}
+        <i className="fa fa-key" /> Verify your account{" "}
+      </h4>
+      <p>Please check your mail, and enter your code in hese</p>
+      <div className="logout-form">
+        <form method="post" className="again-login">
+          <input type="number" placeholder="Enter your code..." autoComplete="off" />
+          <div className="d-flex align-items-center justify-content-center mt-3">
+            <button type="submit" onClick={props.changeType}>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Verify;
