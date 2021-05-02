5f242de3a9455aa93bbf25491827f4a65da5381a
manhdg98 <manhdg98@gmail.com>
2021-05-02 19:15:55 +0700

    - Add env file nodejs

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-console.log(`Node environment: ${process.env.NODE_ENV}`)
-if (process.env.NODE_ENV === 'production') {
-  // this will be setup for different environments:
-  require('dotenv').config()
-} else {
-  require('dotenv').config()
-}
+require("dotenv").config();
+console.log(`Node environment: ${process.env.NODE_ENV}`);
+require("module-alias/register");
 
-require('module-alias/register')
-
-require('./server')
+require("./server");
