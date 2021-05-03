c46c0eefa8d74edc9043e68ce7ef905949cd2a89
manhdg98 <manhdg98@gmail.com>
2021-05-03 20:03:38 +0700

    - Update default page: Login - Remove login page

--- social-front/src/pages/index.tsx
+++ social-front/src/pages/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Page from "components/layout/Page";
+import PageLogin from "components/authen/PageLogin";
 
-const Index = () => <Page></Page>;
+const Index = () => (
+  <Page>
+    <PageLogin />
+  </Page>
+);
 
 export default Index;
