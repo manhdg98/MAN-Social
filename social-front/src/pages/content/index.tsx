8f36e5a12cf2265bc941d49b324182130b45d65b
manhdg98 <manhdg98@gmail.com>
2021-05-13 23:24:06 +0700

    - delete context component

--- social-front/src/pages/content/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Page from "components/layout/Page";
-import PageLogin from "components/authen/PageLogin";
-
-const Index = () => <h1> Manh dep trai </h1>;
-
-export default Index;
