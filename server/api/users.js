fa3f0dcaf8085b2276abc549c9bca9ad290556c0
manhdg98 <manhdg98@gmail.com>
2021-06-13 18:59:40 +0700

    - Update api update-info

--- server/api/users.js
+++ server/api/users.js
@@ -11,52 +11,40 @@ if (process.env.NODE_ENV !== "production") {
   mongoose.set("debug", true);
 }
 
-router
-  .route("/:id/update-password")
-  .post(async (req, res) => {
-    const { oldPassword, newPassword } = req.body;
-    if (newPassword.length >= 8) {
-      const { id } = req.params;
-      const user = await User.findById(id);
-      const isValid = await bcrypt.compare(oldPassword, user.password);
-      if (!isValid) {
-        res.status(400).send("Invalid Password");
-      }
-      user.password = await hashPassword(newPassword);
-      await user.save();
-      res.send(200).send({
-        message: "Update password success. ",
-      });
-    }
-  });
-
-router
-  .route("/:id/update-info")
-  .patch(async (req, res) => {
-    const { gender, state, location, job, about, company, dob, email, username, country, education, phone } = req.body;
-    console.log(req.body);
+router.route("/:id/update-password").post(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  if (newPassword.length >= 8) {
     const { id } = req.params;
     const user = await User.findById(id);
-    Object.keys(user).map(function(key) {
-      
-    });
-    if (email) user.email = email;
-    if (username) user.username = username;
-    if (dob) user.dob = dob;
-    if (company) user.company = company;
-    if (about) user.about = about;
-    if (job) user.job = job;
-    if (location) user.location = location;
-    if (state) user.state = state;
-    if (country) user.country = country;
-    if (gender) user.gender = gender;
-    if (education) user.education = education;
-    if (phone) user.phone = phone;
+    const isValid = await bcrypt.compare(oldPassword, user.password);
+    if (!isValid) {
+      res.status(400).send("Invalid Password");
+    }
+    user.password = await hashPassword(newPassword);
     await user.save();
     res.send(200).send({
       message: "Update password success. ",
     });
+  }
+});
+
+router.route("/:id/update-info").patch(async (req, res) => {
+  const mapInfoUser = { ...req.body };
+  const { id } = req.params;
+  const user = await User.findById(id);
+  for (let key of Object.keys(mapInfoUser)) {
+    if (!user[key]) {
+      throw new Error(`${key} is invalid`);
+    }
+
+    user[key] = mapInfoUser[key];
+  }
+
+  await user.save();
+  res.send(200).send({
+    message: "Update password success. ",
   });
+});
 
 router
   .route("/:id")
@@ -76,16 +64,14 @@ router
   })
   .delete(async (req, res) => {});
 
-router
-  .route("/")
-  .get(async (req, res) => {
-    User.find({}, (err, users) => {
-      let userMap = {};
-      users.forEach((user) => {
-        userMap[user._id] = user;
-      });
-      res.send(userMap);
+router.route("/").get(async (req, res) => {
+  User.find({}, (err, users) => {
+    let userMap = {};
+    users.forEach((user) => {
+      userMap[user._id] = user;
     });
+    res.send(userMap);
   });
+});
 
 module.exports = router;
